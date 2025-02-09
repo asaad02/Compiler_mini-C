e3fb99b8644495bb097f540ae2e83ee3fa31fd86
asaad02 <asaad02@uoguelph.ca>
2025-02-09 16:06:58 -0500

    add test to AST

--- tests/test/test_ast/test7.c
+++ tests/test/test_ast/test7.c
@@ -1,10 +1,8 @@
+int add(int x, int y) {
+    return x + y;
+}
+
 int main() {
-    int a = 10;
-    int b = 20;
-    int c = a + b; // a +B
-    int d = a * b; // Valid expression
-    int e = a / 0;
-    char c1 = 'c';// Invalid character literal (unescaped single quote)
-    char* str = "He said, \"Hi\""; // Invalid string literal unescaped double quote
-    return 0;
+    int result;
+    result = add(3, 4);
 }
