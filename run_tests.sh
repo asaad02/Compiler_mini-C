fc74c8fd247220c8361f7b2784e24625440e35da
asaad02 <asaad02@uoguelph.ca>
2025-02-10 16:52:10 -0500

    Testing

--- run_tests.sh
+++ run_tests.sh
@@ -9,9 +9,10 @@ NC="\033[0m" # No Color
 
 # Set paths
 BUILD_DIR="bin"
-SRC_DIR="./"
-TEST_PARSER_DIR="$SRC_DIR/tests/test/test_Parser"
-TEST_LEXER_DIR="$SRC_DIR/test_Lexing"
+SRC_DIR="."
+TEST_PARSER_DIR="$SRC_DIR/tests/test/test_parser"
+TEST_LEXER_DIR="$SRC_DIR/tests/test/test_lexing"
+TEST_AST_DIR="$SRC_DIR/tests/test/test_ast"
 
 # Function to display a header
 display_header() {
@@ -52,9 +53,11 @@ run_tests() {
       echo -e "${CYAN}Running test: $file${NC}"
       if [[ "$mode" == "parser" ]]; then
         java -cp "$BUILD_DIR" Main1 -parser "$file"
-      elif [[ "$mode" == "lexer" ]]; then
-        java -cp "$BUILD_DIR" Main1 -lexer "$file"
-    fi
+      #elif [[ "$mode" == "lexer" ]]; then
+        #java -cp "$BUILD_DIR" Main1 -lexer "$file"
+      #if [[ "$mode" == "ast" ]]; then
+        #java -cp "$BUILD_DIR" Main2 -ast "$file"
+      fi
 
       # Check if the test passed or failed
       if [ $? -ne 0 ]; then
@@ -79,6 +82,8 @@ main() {
 
   # Run parser tests
   run_tests "$TEST_PARSER_DIR" "parser"
+  #run_tests "$TEST_LEXER_DIR" "parser"
+  #run_tests "$TEST_AST_DIR" "parser"
 
   # Run lexer tests
   #run_tests "$TEST_LEXER_DIR" "lexer"
--- tests/test/test_Lexing/test11.c
+++ tests/test/test_Lexing/test11.c
@@ -1,31 +1,23 @@
 #include "stdio.h"
 
 int main() {
-    // Invalid assignments
-    a = ;       // Missing right-hand value (error)
-    b = 10 + ;  // Incomplete expression (error)
-    c = (10 * );// Unfinished parenthesis (error)
-    d = "hello"; // Invalid type assignment (string to int) (error)
-    e = 5 / 0;  // Division by zero (runtime error, but should be caught by analyzer)
-    f = 5 ** 2; // Invalid operator for exponentiation (error)
-    g = 9 -;    // Incomplete expression (error)
 
-    // Assigning to non-variable
-    10 = a;         // Can't assign to a literal (error)
-    "text" = d;     // Can't assign to a string literal (error)
-    (a + b) = 20;   // Can't assign to an expression (error)
-
-    // Misuse of operators in assignment
-    h = ++;    // Prefix increment without variable (error)
-    i = --;    // Prefix decrement without variable (error)
-    j = = 5;   // Unexpected '=' at start (error)
-    k += 5;    // Use of '+=' without initialization (error)
-    l -= 3;    // Use of '-=' without initialization (error)
+    a = 10 ;       
+    b = 10 + 12 ;  
+    c = (10 * 2);
+    d = "hello";
+ 
 
 
-    // Using operators incorrectly
-    int m = 5 *;  // Incomplete multiplication (error)
-    int n = / 2;  // No left operand for division (error)
+    "text" = d;     
+
+
+    h = ++;    
+
+
+
+    int m = 5 *;  
+    int n = / 2;  
 
 
     return 0;
