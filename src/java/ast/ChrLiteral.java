f92ee5d8ecb44246b050166c2a4551b4397e9fe4
asaad02 <asaad02@uoguelph.ca>
2025-02-12 00:04:27 -0500

    fix the charliteral and escape charecter

--- src/java/ast/ChrLiteral.java
+++ src/java/ast/ChrLiteral.java
@@ -5,9 +5,9 @@ import java.util.List;
 // a character literal ('a').
 public final class ChrLiteral extends Expr {
   // The character literal value
-  public final char value;
+  public final String value;
 
-  public ChrLiteral(char value) {
+  public ChrLiteral(String value) {
     this.value = value;
   }
 
