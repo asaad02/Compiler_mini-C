e42b46a4795f041b9140a56c0050390262b3ae93
asaad02 <asaad02@uoguelph.ca>
2025-02-17 02:03:23 -0500

    add get parameter from function symbol

--- src/java/sem/FunSymbol.java
+++ src/java/sem/FunSymbol.java
@@ -1,37 +1,47 @@
 package sem;
 
 import ast.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * funSymbol stores function declarations and definitions. ensures correct handling of declaration
  * and definition cases.
  */
 public class FunSymbol extends Symbol {
-  // Stores the function declaration
   public FunDecl decl;
-  // Stores the function definition
   public FunDef def;
+  public Type type;
 
   public FunSymbol(FunDecl decl) {
     super(decl.name);
     this.decl = decl;
-    // Initially no definition
     this.def = null;
+    this.type = decl.type;
   }
 
   public FunSymbol(FunDef def) {
     super(def.name);
     this.def = def;
-    // Initially no declaration
     this.decl = null;
+    this.type = def.type;
   }
 
-  // storing both declaration and definition if both exist
   public void setDeclaration(FunDecl decl) {
     this.decl = decl;
+    this.type = decl.type;
   }
 
   public void setDefinition(FunDef def) {
     this.def = def;
+    this.type = def.type;
+  }
+
+  public List<Type> getParamTypes() {
+    return (def != null)
+        ? def.params.stream().map(param -> param.type).collect(Collectors.toList())
+        : (decl != null)
+            ? decl.params.stream().map(param -> param.type).collect(Collectors.toList())
+            : List.of();
   }
 }
