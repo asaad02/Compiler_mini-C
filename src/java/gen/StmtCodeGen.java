e81d6049b2b4a06ae507b152b1f1f68448fef6df
asaad02 <asaad02@uoguelph.ca>
2025-03-11 16:04:34 -0400

    fix function return

--- src/java/gen/StmtCodeGen.java
+++ src/java/gen/StmtCodeGen.java
@@ -140,8 +140,22 @@ public class StmtCodeGen extends CodeGen {
       throw new IllegalStateException("[StmtCodeGen] Continue outside loop");
     }
     AssemblyProgram.TextSection text = asmProg.getCurrentTextSection();
+    // Ensure stack is aligned before jumping back to loop start
+    Register tempReg = Register.Virtual.create();
+    Register negTempReg = Register.Virtual.create();
+
+    text.emit(OpCode.ANDI, tempReg, Register.Arch.sp, 15); // tempReg = sp % 16
+    text.emit(
+        OpCode.BEQ,
+        tempReg,
+        Register.Arch.zero,
+        loopStack.peek().start); // If already aligned, jump
+    // negTempReg = -tempReg
+    text.emit(OpCode.SUBU, negTempReg, Register.Arch.zero, tempReg);
+    // Correct misalignment
+    text.emit(OpCode.ADDU, Register.Arch.sp, Register.Arch.sp, negTempReg);
     // Jump to loop start
-    text.emit(OpCode.J, loopStack.peek().start); // Jump to loop start
+    text.emit(OpCode.J, loopStack.peek().start);
   }
 
   /** Handles break statements by jumping to the end of the nearest enclosing loop. */
